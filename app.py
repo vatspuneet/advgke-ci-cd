60a27453f9ef01158f8a333fa8e98346dab08d02
puneet vats <vats.puneet@gmail.com>
2026-01-28 10:17:03 +0300

    hello from class

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from GKE! part 4523423423423423'
+    return 'Hello from GKE! part -- Hello from Class -----'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
